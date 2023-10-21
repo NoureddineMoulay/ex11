f6c13e45d7a6ad2b94700b89c7038bc624f12699
noureddine <uixa801@gmail.com>
2023-10-21 14:04:34 +0100

    la premiere commit

---
+++ Ex11.java
@@ -0,0 +1,32 @@
+package ex11;
+import static java.lang.Math.sqrt;
+
+import java.util.Scanner;
+
+public class Ex11  {
+  public static void Racine() {
+	  double r ;
+	  int n ;
+	  do {
+		  Scanner p=new Scanner(System.in);
+		  System.out.println("entrer un entier positif:");
+		 n=p.nextInt();
+		  if(n>0) {
+		  r=sqrt(n);
+		  System.out.println("la racine est :"+r);
+		  }
+		  else if(n<0) {
+			 System.out.println("SVP positif") ;
+		  }
+		  }while( n!=0);
+	  System.out.println("Au revoir") ;
+	  
+	  
+	  
+	  
+  }
+	public static void main(String[] args) {
+			Racine();
+	}
+
+}
